340a4c298dc3c694a133d1626548ddc29965e20d
fbara <fbara1@yahoo.com>
2021-10-15 14:22:14 -0500

    Initial Commit

---
+++ VBall ScoreMaster 5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VBall ScoreMaster 5
+//
+//  Created by Frank Bara on 10/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VBall ScoreMaster 5/VBall_ScoreMaster_5App.swift
@@ -0,0 +1,17 @@
+//
+//  VBall_ScoreMaster_5App.swift
+//  VBall ScoreMaster 5
+//
+//  Created by Frank Bara on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct VBall_ScoreMaster_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
