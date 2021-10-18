8b2492ef11c70686a8a55330f775607011f18ec5
fbara <fbara1@yahoo.com>
2021-10-18 11:14:48 -0500

    Add asset images for tabs.

--- VBall ScoreMaster 5/ContentView.swift
+++ VBall ScoreMaster 5/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                //Text("Scoreboard")
+                    .padding()
+                Spacer()
+                HStack {
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName:
+                                "vballnet", tabName: "Score")
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName:
+                                "settings", tabName: "Settings")
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName:
+                                "info.circle", tabName: "Support")
+                    
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height / 8)
+                .background(Color("TabBarBackground"))
+                .shadow(radius: 2)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TabBarIcon: View {
+    let width, height: CGFloat
+    let systemIconName: String
+    let tabName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text(tabName)
+                .font(.footnote)
+            Spacer()
+        }
     }
 }
